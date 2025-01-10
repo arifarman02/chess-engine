4f9b7ca79c99988072a9b67b1f951c97bcb9c574
arifarman02 <arifarman1820@gmail.com>
2025-01-10 20:14:33 +0000

    Started work on pieces

---
+++ assets.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void pawn(){
+    int piece_value = 1;
+}
+
+void king(){
+}
+
+void bishop(){
+    int piece_value = 3;
+}
+
+void knight(){
+    int piece_value = 3;
+}
+
+void castle(){
+    int piece_value = 5;
+}
+
+void queen(){
+    int piece_value = 9;
+}
